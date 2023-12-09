318a34fb7a33c92515e2d2e9dff5c665fe8cb0c6
Jillian Conway <Jconway4@terpmail.umd.edu>
2023-12-09 16:36:50 -0500

    added comments for design

--- final_project.py
+++ final_project.py
@@ -8,4 +8,16 @@ suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 #assigns decks as a list of tuples 
 deck = [(suit, rank) for suit in suits for rank in ranks]
-print(deck)
+print(len(deck))
+
+#shuffle deck function
+
+#deal function
+#pass in a deck and gets back two hands
+
+# main
+
+#rules of war
+#play a round, compare cards
+
+#create scoreboard (numpy)
