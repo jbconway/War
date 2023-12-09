4836bff4c1990cc6567ebf70b95675e4cd7eaa1c
Jillian Conway <Jconway4@terpmail.umd.edu>
2023-12-09 16:19:29 -0500

    initial commit

---
+++ final_project.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+#suits and ranks in tuples 
+suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+#assigns decks as a list of tuples 
+deck = [(suit, rank) for suit in suits for rank in ranks]
